9e278546bb89dbf420e4bf9d9cbdff9042bc63ee
Debian <admin@ip-172-31-40-236.us-east-2.compute.internal>
2020-07-22 21:22:23 +0000

    ?

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+$databases['default']['default'] = array(
+	'database' => 'drupal_randy_cms',
+	'username' => 'admin',
+	'password' => 'Ezmb8E8kHPy6bp7b',
+	'prefix' => '',
+	'host' => 'tca-db-cb7hm0kzulws.us-east-2.rds.amazonaws.com',
+	'port' => '3306',
+	'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+	'driver' => 'mysql'
+);
